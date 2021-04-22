dfc7f9ca130171cbb1c4a8212fc437d3a2e1eff2
AmienulRana <amienulrana@gmail.com>
2021-04-22 15:32:28 +0700

    =menjalankan fungsi aktfi pada nav link

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,10 @@
 const navLink = document.querySelectorAll('.nav-link')
 
 navLink.forEach(nl => {
-    nl.addEventListener('click', function () {
-        nl.classList.add('aktif')
+    nl.addEventListener('click', function (e) {
+        navLink.forEach(nl => {
+            nl.classList.remove('aktif')
+        })
+       e.target.classList.add('aktif')
     })
 })
