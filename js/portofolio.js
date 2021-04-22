fcfd669d908d049262aa4cfef4ab6c84a3006163
AmienulRana <amienulrana@gmail.com>
2021-04-22 15:46:06 +0700

    =Membuat file portofolio.js

---
+++ js/portofolio.js
@@ -0,0 +1,20 @@
+const portofolio = [
+    {
+        img: 'hefa-store.png',
+        namaWeb:'Hefa Store'
+    },
+    {
+        img: 'healthy-food.png',
+        namaWeb:'Healthy Food'
+    }
+]
+ 
+portofolio.forEach(img => {
+
+})
+
+function getPortofolio(img){
+    return (
+        ``
+    )
+}
