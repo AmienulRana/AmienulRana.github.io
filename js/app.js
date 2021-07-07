4c2766185b4c18f697740ddae9f76b1c54ca1c77
AmienulRana <amienulrana@gmail.com>
2021-07-07 20:05:12 +0700

    =mengubah warna bacgkround dan memetikan animasi css di  bagianskill

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+
+// function posFade(fade, position) {
+//   const winHeight = window.innerHeight / 1.2;
+//   const posUp = fade.getBoundingClientRect().top;
+//   if (posUp < winHeight) {
+//     fade.classList.add(position);
+//   } else {
+//     fade.classList.remove(position);
+//   }
+// }
+// function scrollFade() {
+//   const about = document.querySelector(".about");
+//   const portofolio = document.querySelector(".portofolio");
+//   const skills = document.querySelector(".skills");
+//   posFade(about, "aktif");
+//   posFade(portofolio, "aktif");
+//   posFade(skills, "aktif");
+// }
+// scrollFade();
+// const about = document.querySelector(".about");
+// const portofolio = document.querySelector(".portofolio");
+// const skills = document.querySelector(".skills");
+// window.addEventListener("scroll", function () {
+//   const posAbout = about.getBoundingClientRect().top;
+//   const posPortofolio = portofolio.getBoundingClientRect().top;
+//   const posSkills = skills.getBoundingClientRect().top;
+//
+//   console.log(posAbout, posPortofolio, posSkills);
+// });
